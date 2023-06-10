4097c470feed4783badf13a2717144fe03a61249
dilipsanapinb <dilipinbsanap@gmail.com>
2023-06-10 13:31:51 +0530

    BooTrip app backend added updated

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const { connection } = require('./config/db');
 
 const { bookRoute } = require('./routes/book.route');
 
-const cors = require("cors");
 
-app.use(cors());
 
 const app = express();
 
